06815420347ef2fff34d90bace3f87e5fdd4e19d
Enzo <en.lecoq2000@gmail.com>
2023-09-11 16:31:45 +0200

    fonctionnel

--- js/Shifumi.js
+++ js/Shifumi.js
@@ -1,28 +1,69 @@
 let buttons = document.querySelectorAll("button")
-let result = document.getElementsByClassName("resultat")
+let result = document.querySelector(".resultat")
+const resulttxt = ["Joueur :","robot :","resultat :"]
+const resultType = ["pierre","feuille","ciseaux"]
 
+console.log(result)
+
+
+for(let x=0;x< resulttxt.length;x++)
+    {
+        let resulttab = document.createElement("p");
+        resulttab.innerText = resulttxt[x];
+        result.appendChild(resulttab);
+            
+    }
+
+let   nodevalue = document.querySelectorAll("p")
+
+//const resulttab = result.createElement("p");
     for (let index = 0; index < buttons.length; index++) {
         
 
+
+
         buttons[index].addEventListener('click',()=>{
-            alert("button clicked! ")
-            const bot = Math.random(0,buttons.length);
-            console.log(bot)
-            
+            //alert("button clicked! "+ index)
+            console.log("j :" + index)
+            const bot = Math.floor(Math.random(0,buttons.length)*3);
+            console.log(bot);
+            nodevalue[0].innerText = resulttxt[0]+  resultType[index];
+            nodevalue[1].innerText = resulttxt[1]+  resultType[bot];
 
-            if( index == bot)
-            {
-                result.textContent = "égalité";
-            }
-            else 
-            
-            result.textContent = "hello world !";
+            nodevalue[2].innerText = resulttxt[2]+  getresultat(index,bot);
 
             
-            
+                //resulttab[3].innerText = resulttxt[index] + ""+index;
+
+                //nodevalue[2].innerText = resulttxt[1]+""+( index - bot);
+                
 
 
         })
         
+        
     };
+
+    function getresultat(index,bot)
+    {
+        result = (bot - index ) % 3 ;
+
+        console.log(bot + " j = "+ index+" modulo "+ result);
+        if (result == 0 )//si eégalité
+        {
+            return "égualité "
+        }
+
+        else if (result == 1 || result == 2 )
+        {
+
+            return "bot gagne"
+        }
+
+        else
+        {
+            return " vous gagnez"
+
+        }
+    }
     //reecrire dans le dom
